aeed3b4bd07a510476d99c9729d01f2babc4f88b
EnixCoda <enixcoda@gmail.com>
2023-07-05 00:40:17 +0800

    refactor: optimize getData

--- src/utils/getData.ts
+++ src/utils/getData.ts
@@ -1,10 +1,10 @@
-import { NodeObject } from "force-graph";
+import { NodeObject as ForceGraphNodeObject } from "force-graph";
 import { DependencyEntry } from "../services/serializers";
 import { safeMapGet } from "./general";
 
 type NodeId = string;
 
-interface SNodeObject extends NodeObject {
+interface NodeObject extends ForceGraphNodeObject {
   id: NodeId;
 }
 
@@ -31,133 +31,114 @@ export const getData = (
   { dependantMap, dependencyMap }: PreparedData,
   {
     roots,
-    excludes = new Set(),
     leave,
+    excludes = new Set(),
     preventCycle,
     dagPruneMode = "less roots",
   }: {
     roots?: Set<NodeId>;
-    excludes?: Set<NodeId>;
     leave?: Set<NodeId>;
+    excludes?: Set<NodeId>;
     preventCycle?: boolean;
     dagPruneMode?: DAGPruneMode;
   } = {}
 ) => {
-  const traversedNodes = new Set<SNodeObject["id"]>();
-  const nodes = new Map<NodeId, SNodeObject>();
-  const links = new Map<NodeId, Set<NodeId>>();
+  // Traverse through dependency map and dependent map starting from roots and leave to explore the graph
+  const traverse = (startNodes: Set<NodeId>, map: Map<NodeId, Set<NodeId>>) => {
+    const traversedNodes = new Set<NodeObject["id"]>();
+    const cycles: NodeId[][] = [];
+    const traverseData = (node: NodeId, stack: NodeId[] = []) => {
+      if (excludes.has(node)) return;
 
-  type TraverseDirection = "dependant" | "dependency";
-
-  const cycles: string[][] = [];
-  const traverseData = (id: NodeId, direction: TraverseDirection, stack: NodeId[] = []) => {
-
-    if (stack.includes(id)) {
-      cycles.push(stack.slice(stack.indexOf(id)));
-      if (preventCycle) return;
-    }
-    if (excludes.has(id)) return;
-
-    if (traversedNodes.has(id)) return;
-    traversedNodes.add(id);
-
-    safeMapGet(nodes, id, () => ({ id }));
-
-    if (direction === "dependency") {
-      for (const dependency of dependencyMap.get(id)?.values() || []) {
-        if (excludes.has(dependency)) continue;
-        safeMapGet(links, id, () => new Set()).add(dependency);
-        traverseData(dependency, direction, [...stack, id]);
+      if (stack.includes(node)) {
+        cycles.push(stack.slice(stack.indexOf(node)));
+        if (preventCycle) return;
       }
-    }
 
-    if (direction === "dependant") {
-      for (const dependant of dependantMap.get(id)?.values() || []) {
-        if (excludes.has(dependant)) continue;
-        safeMapGet(links, dependant, () => new Set()).add(id);
-        traverseData(dependant, direction, [...stack, id]);
+      if (traversedNodes.has(node)) return;
+      traversedNodes.add(node);
+
+      for (const next of map.get(node)?.values() || []) {
+        if (excludes.has(next)) continue;
+        traverseData(next, stack.concat(node));
       }
-    }
+    };
+
+    for (const r of startNodes) traverseData(r);
+    return {
+      nodes: traversedNodes,
+      cycles,
+    };
   };
+  const [traversedFromRoots, traversedFromLeave] = [
+    roots && traverse(roots, dependantMap),
+    leave && traverse(leave, dependencyMap),
+  ];
 
-    traversedNodes.clear();
-  const traverse = (startNodes: Set<NodeId>, direction: "dependant" | "dependency") => {
-    for (const r of startNodes) traverseData(r, direction);
-    return new Set(traversedNodes);
-  };
-
-  const [downTraversedNodes, upTraversedNodes] =
-    dagPruneMode === "less roots"
-      ? [traverse(roots || new Set(), "dependant"), traverse(leave || new Set(), "dependency")]
-      : [traverse(leave || new Set(), "dependency"), traverse(roots || new Set(), "dependant")].reverse();
-
-  // Prune network if roots and leave are not same
-  // This might not be very accurate
-  if (roots && leave) {
-    const outOfRangeNodes = new Set<NodeId>();
-    for (const [, node] of nodes) {
-      if (!(upTraversedNodes.has(node.id) && downTraversedNodes.has(node.id))) {
-        nodes.delete(node.id as NodeId);
-        outOfRangeNodes.add(node.id as NodeId);
-      }
-    }
-
-    for (const [from, tos] of links) {
-      if (outOfRangeNodes.has(from)) {
-        links.delete(from);
-      } else {
-        for (const to of tos) {
-          if (outOfRangeNodes.has(to)) {
-            tos.delete(to);
-          }
-        }
-      }
-    }
+  // Prune if both roots and leave are specified
+  // Example usage:
+  //   Check out all import statements of 'react'(as leaf) starting from 'src/page/Home.js'(as root)
+  // If there is a cycle in a fork branch, exclude it
+  const nodes = new Set<NodeId>();
+  if (traversedFromRoots && traversedFromLeave) {
+    for (const node of traversedFromRoots.nodes) if (traversedFromLeave.nodes.has(node)) nodes.add(node);
+  } else {
+    for (const node of traversedFromRoots?.nodes || []) nodes.add(node);
+    for (const node of traversedFromLeave?.nodes || []) nodes.add(node);
   }
 
-  // remove cycles
-  {
-    const traversedNodes = new Set<NodeId>();
-    for (const id of nodes.keys()) {
-      if (traversedNodes.has(id)) continue;
-
-      const go = (id: NodeId, stack: NodeId[]) => {
-        if (traversedNodes.has(id)) return;
-        traversedNodes.add(id);
-
-        if (stack.includes(id)) return;
-        stack.push(id);
-
-        const targets = links.get(id);
-        if (targets) {
-          for (const target of targets.values()) {
-            if (stack.includes(target)) {
-              targets.delete(target); // not link from id to target to remove cycles
-              continue;
-            } else {
-              go(target, stack);
-            }
-          }
-        }
-        stack.pop();
-      };
-
-      go(id, []);
-    }
-  }
-
+  // remove cycles, either
+  // 1. duplicated
+  // 2. cycle not accessible from both roots and leave
+  const cycles: NodeId[][] = [];
+  for (const cycle of traversedFromRoots?.cycles || []) if (cycle.every((node) => nodes.has(node))) cycles.push(cycle);
+  for (const cycle of traversedFromLeave?.cycles || []) if (cycle.every((node) => nodes.has(node))) cycles.push(cycle);
+  // remove duplicated cycles
   const separator = "|";
   const orderedCycles = cycles.map((cycle) => {
-    const first = cycle.indexOf(cycle.reduce((earliest, item) => (earliest < item ? earliest : item)));
-    return cycle.slice(first).concat(cycle.slice(0, first));
+    const headOfCycle = cycle.reduce((earliest, item) => (earliest < item ? earliest : item));
+    const indexOfHead = cycle.indexOf(headOfCycle);
+    return indexOfHead === 0 ? cycle : cycle.slice(indexOfHead).concat(cycle.slice(0, indexOfHead));
   });
   const deduplicatedCycles = Array.from(new Set(orderedCycles.map((cycle) => cycle.join(separator)))).map((cycle) =>
     cycle.split(separator)
   );
 
+  const links = new Map<NodeId, Set<NodeId>>();
+  for (const node of nodes) {
+    dependencyMap.get(node)?.forEach((dependency) => {
+      if (nodes.has(dependency)) safeMapGet(links, node, () => new Set()).add(dependency);
+    });
+    dependantMap.get(node)?.forEach((dependant) => {
+      if (nodes.has(dependant)) safeMapGet(links, dependant, () => new Set()).add(node);
+    });
+  }
+
+  if (preventCycle) {
+    // Remove links to prevent cycle to enable DAG rendering
+    // For example, if there was a cycle of `a -> b -> c -> a`.
+    // If `a` is root, and prune for less roots, output would be `a -> b -> c`.
+    // If `a` is leave, and prune for less leave, output would be `b -> c -> a`.
+    cycles.forEach((cycle) => {
+      cycle.forEach((node, index) => {
+        if (roots && dagPruneMode === "less roots") {
+          if (roots.has(node)) {
+            const last = cycle[(index === 0 ? cycle.length : index) - 1];
+            safeMapGet(links, last, () => new Set()).delete(node);
+          }
+        } else if (leave && dagPruneMode === "less leave") {
+          if (leave.has(node)) {
+            const next = cycle[(index === cycle.length ? 0 : index) + 1];
+            safeMapGet(links, node, () => new Set()).delete(next);
+          }
+        }
+      });
+    });
+  }
+
   return {
     cycles: deduplicatedCycles,
-    nodes: [...nodes.values()],
+    nodes: [...nodes.values()].map((id) => ({ id })),
     links: [...links.entries()]
       .map(([source, targets]) => [...targets.values()].map((target) => ({ source, target })))
       .flat(),
