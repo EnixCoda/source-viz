c24138cea634dc0c1c41caebe3fcbf2109303471
EnixCoda <enixcoda@gmail.com>
2023-07-05 01:35:15 +0800

    refactor: support helper text in checkbox view

--- src/hooks/view/useCheckboxView.tsx
+++ src/hooks/view/useCheckboxView.tsx
@@ -1,17 +1,22 @@
-import { Checkbox, CheckboxProps, FormControl } from "@chakra-ui/react";
+import { Checkbox, CheckboxProps, FormControl, FormHelperText } from "@chakra-ui/react";
 import * as React from "react";
 import { useView } from "./useView";
 
-export function useCheckboxView(label: React.ReactNode, defaultValue: boolean, extraProps?: CheckboxProps) {
+export function useCheckboxView(
+  label: React.ReactNode,
+  defaultValue: boolean,
+  { checkboxProps, helperText }: { checkboxProps?: CheckboxProps; helperText?: React.ReactNode } = {}
+) {
   return useView(
     defaultValue,
     (checked, setChecked) => (
       <FormControl>
-        <Checkbox isChecked={checked} onChange={(e) => setChecked(e.target.checked)} {...extraProps}>
+        <Checkbox isChecked={checked} onChange={(e) => setChecked(e.target.checked)} {...checkboxProps}>
           {label}
         </Checkbox>
+        {helperText && <FormHelperText>{helperText}</FormHelperText>}
       </FormControl>
     ),
-    [label, extraProps]
+    [label, checkboxProps]
   );
 }
