48e6bb8b5941198ee2a8bb16c88143a14a8d768b
EnixCoda <enixcoda@gmail.com>
2023-07-14 17:34:03 +0800

    feat: auto resize directory column on double click

--- src/components/FileExplorer/column/Directory.tsx
+++ src/components/FileExplorer/column/Directory.tsx
@@ -1,5 +1,6 @@
 import { Button, HStack, List, ListItem, Spinner, Text } from "@chakra-ui/react";
 import * as React from "react";
+import { IIFC } from "react-iifc";
 import { Size2D, useResizeHandler } from "../../../hooks/useResizeHandler";
 import { MetaFilter } from "../../../services";
 import { getPatternsMatcher, run } from "../../../utils/general";
@@ -25,6 +26,18 @@ export function ColumnDirectory({
   const { onPointerDown } = useResizeHandler(size, setSize);
   const [files, setFiles] = React.useState<FileSystemHandle[] | null>(null);
   const ref = React.useRef<HTMLOListElement | null>(null);
+
+  const registryRef = React.useRef(new Set<() => Size2D[0]>());
+  const makeAllItemsVisible = React.useCallback(() => {
+    if (!ref.current) return;
+    let maxGrowth = 0;
+    for (const getWidthToGrow of registryRef.current) {
+      const growth = getWidthToGrow();
+      if (growth > maxGrowth) maxGrowth = growth;
+    }
+    // the extra 1px for fixing edge cases that text still truncated after resizing
+    setSize(([width, height]) => [Math.ceil(width + maxGrowth) + 1, height]);
+  }, []);
 
   React.useEffect(() => {
     if (files && ref.current && scrollIntoView) ref.current.scrollIntoView();
@@ -84,22 +97,44 @@ export function ColumnDirectory({
               justifyContent="flex-start"
               color={
                 isItemExcluded(file.name)
-                  ? "gray.400"
+                  ? "gray.500"
                   : isItemIncluded(file.name)
-                  ? "orange.500"
+                  ? "green.500"
                   : file.kind === "file"
-                  ? "gray.400"
+                  ? "gray.500"
                   : undefined
               }
             >
-              <Text title={file.name} overflow="hidden" textOverflow="ellipsis">
-                {file.kind === "directory" ? "📁" : "📄"} {file.name}
-              </Text>
+              <IIFC>
+                {() => {
+                  // eslint-disable-next-line react-hooks/rules-of-hooks
+                  const textRef = React.useRef<HTMLSpanElement>(null);
+                  // eslint-disable-next-line react-hooks/rules-of-hooks
+                  React.useEffect(() => {
+                    // register
+                    const measure = () => {
+                      if (!textRef.current) return 0;
+                      return textRef.current.scrollWidth - textRef.current.clientWidth;
+                    };
+                    registryRef.current.add(measure);
+
+                    // unregister
+                    return () => {
+                      registryRef.current.delete(measure);
+                    };
+                  }, []);
+                  return (
+                    <Text ref={textRef} as="span" title={file.name} overflow="hidden" textOverflow="ellipsis">
+                      {file.kind === "directory" ? "📁" : "📄"} {file.name}
+                    </Text>
+                  );
+                }}
+              </IIFC>
             </Button>
           </ListItem>
         ))}
       </List>
-      <HorizontalResizeHandler onPointerDown={onPointerDown} />
+      <HorizontalResizeHandler onPointerDown={onPointerDown} onDoubleClick={makeAllItemsVisible} />
     </>
   );
 }
