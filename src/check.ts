65b57ee7b557cc5673c8a9f805c33b1b06d5e0d6
EnixCoda <enixcoda@gmail.com>
2024-04-23 23:22:42 +0800

    build: fix build

--- src/check.ts
+++ src/check.ts
@@ -1,24 +1,24 @@
-type Expect<T extends true> = T;
-type ExpectTrue<T extends true> = T;
-type ExpectFalse<T extends false> = T;
-type IsTrue<T extends true> = T;
-type IsFalse<T extends false> = T;
+export type Expect<T extends true> = T;
+export type ExpectTrue<T extends true> = T;
+export type ExpectFalse<T extends false> = T;
+export type IsTrue<T extends true> = T;
+export type IsFalse<T extends false> = T;
 
-type Equal<X, Y> = (<T>() => T extends X ? 1 : 2) extends <T>() => T extends Y ? 1 : 2 ? true : false;
-type NotEqual<X, Y> = true extends Equal<X, Y> ? false : true;
+export type Equal<X, Y> = (<T>() => T extends X ? 1 : 2) extends <T>() => T extends Y ? 1 : 2 ? true : false;
+export type NotEqual<X, Y> = true extends Equal<X, Y> ? false : true;
 
 // https://stackoverflow.com/questions/49927523/disallow-call-with-any/49928360#49928360
-type IsAny<T> = 0 extends 1 & T ? true : false;
-type NotAny<T> = true extends IsAny<T> ? false : true;
+export type IsAny<T> = 0 extends 1 & T ? true : false;
+export type NotAny<T> = true extends IsAny<T> ? false : true;
 
-type Debug<T> = { [K in keyof T]: T[K] };
-type MergeInsertions<T> = T extends object ? { [K in keyof T]: MergeInsertions<T[K]> } : T;
+export type Debug<T> = { [K in keyof T]: T[K] };
+export type MergeInsertions<T> = T extends object ? { [K in keyof T]: MergeInsertions<T[K]> } : T;
 
-type Alike<X, Y> = Equal<MergeInsertions<X>, MergeInsertions<Y>>;
+export type Alike<X, Y> = Equal<MergeInsertions<X>, MergeInsertions<Y>>;
 
-type ExpectExtends<VALUE, EXPECTED> = EXPECTED extends VALUE ? true : false;
-type ExpectValidArgs<FUNC extends (...args: any[]) => any, ARGS extends any[]> = ARGS extends Parameters<FUNC>
+export type ExpectExtends<VALUE, EXPECTED> = EXPECTED extends VALUE ? true : false;
+export type ExpectValidArgs<FUNC extends (...args: any[]) => any, ARGS extends any[]> = ARGS extends Parameters<FUNC>
   ? true
   : false;
 
-type UnionToIntersection<U> = (U extends any ? (k: U) => void : never) extends (k: infer I) => void ? I : never;
+export type UnionToIntersection<U> = (U extends any ? (k: U) => void : never) extends (k: infer I) => void ? I : never;
